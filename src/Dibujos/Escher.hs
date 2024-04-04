24e305fec5099e457601a17d1596c94961fad2aa
Ramiro Lugo <ramiro.lugo@mi.unc.edu.ar>
2024-04-04 09:37:19 -0300

    refactor: Revert (Daián Exequiel Giménez  committed  28b59e7)

--- src/Dibujos/Escher.hs
+++ src/Dibujos/Escher.hs
@@ -1,29 +1,8 @@
 module Dibujos.Escher where
 
-import Dibujo (Dibujo, figura, juntar, apilar, rot45, rotar, encimar, espejar)
-import FloatingPic(Conf(..), Output, half, zero)
-import qualified Graphics.Gloss.Data.Point.Arithmetic as V
-import Graphics.Gloss ( Picture, blue, red, color, pictures, rectangleSolid, scale, text, black, white, translate)
+-- Supongamos que eligen.
+type Escher = Bool
 
-
-data Color = Azul deriving (Show, Eq)
-
-data EscherTriangleSinColor = Triangulo   deriving (Show, Eq)
-
-colorear :: Color -> Picture -> Picture
-colorear Azul = color blue
-
-
-type Escher = ( EscherTriangleSinColor, Color)
-
-cuarteto :: Dibujo Escher -> Dibujo Escher -> Dibujo Escher -> Dibujo Escher  -> Dibujo Escher
-cuarteto p q r s = apilar 1 1 (juntar 1 1 p q) (juntar 1 1 r s)
-
-ciclo :: Dibujo Escher -> Dibujo Escher
-ciclo p = cuarteto p (rotar p) (rotar(rotar p)) (rotar(rotar(rotar p)))
-
-ov :: Picture -> Picture -> Picture
-ov p q = pictures [p, q]
 -- El dibujo u.
 dibujoU :: Dibujo Escher -> Dibujo Escher
 dibujoU p = undefined 
@@ -34,13 +13,11 @@ dibujoT p = undefined
 
 -- Esquina con nivel de detalle en base a la figura p.
 esquina :: Int -> Dibujo Escher -> Dibujo Escher
-esquina 1 f = cuarteto blank blank blank (dibujoU f)
-esquina 2 f = cuarteto (esquina 1 f) (lado 1 f) rotar(lado 1 f) (dibujoU f)
+esquina n p = undefined
 
 -- Lado con nivel de detalle.
 lado :: Int -> Dibujo Escher -> Dibujo Escher
-lado 1 f = cuarteto blank blank (rotar (dibujoT f))  (dibujoT f)
-lado 2 f = cuarteto (lado 1 f) (lado 1 f) (rotar f) f
+lado n p = undefined
 
 -- Por suerte no tenemos que poner el tipo!
 noneto p q r s t u v w x = undefined

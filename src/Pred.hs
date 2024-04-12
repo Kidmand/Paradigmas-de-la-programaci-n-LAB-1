8dd9e23b516beee6ea295d0cd9d2d8c884e41202
Ramiro Lugo <ramiro.lugo@mi.unc.edu.ar>
2024-04-12 13:36:50 -0300

    style: Mejore la organizacion de Pred.hs

--- src/Pred.hs
+++ src/Pred.hs
@@ -5,46 +5,20 @@ module Pred (
 
 import Dibujo
 
+-----------------------------------------------------------------------------------------------------------------------
+-- NOTE: Definimos el tipo de dato Pred, que es una función que recibe un parámetro y devuelve un booleano.
 type Pred a = a -> Bool
+-----------------------------------------------------------------------------------------------------------------------
 
--- -- Ejemplo de tipo de dato para probar las funciones.
--- data Figura = Cuadrado Int | Circulo Int | Triangulo Int Int Int
---   deriving (Show, Eq)
-
--- -- Predicado que indica si una figura es un cuadrado.
--- esCuadrado :: Pred Figura
--- esCuadrado (Cuadrado _) = True
--- esCuadrado _ = False
--- -- Ejemplo de uso: 
--- -- ghci> esCuadrado (Cuadrado 1)
--- -- True
--- -- ghci> esCuadrado (Circulo 1)
--- -- False
-
--- -- Ejemplo de dibujo para probar las funciones.
--- circulo n = figura (Circulo n)
--- cuadrado n = figura (Cuadrado n)
--- dibujo_de_ejemplo_1 = apilar 1 1 (circulo 1) (espejar (circulo 1))
--- dibujo_de_ejemplo_2 = juntar 1 1 (rot45 (cuadrado 1)) (rotar (cuadrado 1))
--- dibujo_de_ejemplo_3 = encimar (dibujo_de_ejemplo_2) (dibujo_de_ejemplo_1)
--- dibujo_de_ejemplo_4 = juntar 1 1 (rot45 (circulo 1)) (rotar (circulo 1))
--- dibujo_de_ejemplo_5 = encimar (dibujo_de_ejemplo_1) (dibujo_de_ejemplo_4)  
-
-
--- -- Funcion de transformacion. 
--- cuadradoACirculo :: Figura -> Dibujo Figura
--- cuadradoACirculo (Cuadrado n) = figura (Circulo (n*2))
--- cuadradoACirculo f = figura f
-
--- -- Compo probar: "cambiar esCuadrado cuadradoACirculo dibujo_de_ejemplo"
-
+-----------------------------------------------------------------------------------------------------------------------
+-- Auxiliar de cambiar.
 pf :: Pred a -> (a -> Dibujo a) -> a -> Dibujo a
 pf pre f x
   | pre x = f x
   | otherwise = figura x
 
--- Dado un predicado sobre básicas, cambiar todas las que satisfacen
--- el predicado por la figura básica indicada por el segundo argumento.
+-- NOTE: Dado un predicado sobre básicas, cambiar todas las que satisfacen
+--       el predicado por la figura básica indicada por el segundo argumento.
 cambiar :: Pred a -> (a -> Dibujo a) -> Dibujo a -> Dibujo a
 cambiar pre f d = foldDib 
                       (pf pre f) 
@@ -59,10 +33,14 @@ cambiar pre f d = foldDib
                       encimar
                       d
 
+-----------------------------------------------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------------------------------------------
+-- Auxiliar de anyDib.
 orIgnoreFloat :: Float -> Float -> Bool -> Bool -> Bool
 orIgnoreFloat _ _ b1 b2 = b1 || b2
 
--- Alguna básica satisface el predicado.
+-- NOTE: Alguna básica satisface el predicado.
 anyDib :: Pred a -> Dibujo a -> Bool
 anyDib pre d = foldDib
                   pre
@@ -73,11 +51,14 @@ anyDib pre d = foldDib
                   (orIgnoreFloat)
                   (||)
                   d
+-----------------------------------------------------------------------------------------------------------------------
 
+-----------------------------------------------------------------------------------------------------------------------
+-- Auxiliar de allDib.
 andIgnoreFloat :: Float -> Float -> Bool -> Bool -> Bool
 andIgnoreFloat _ _ b1 b2 = b1 && b2
 
--- Todas las básicas satisfacen el predicado.
+-- NOTE: Todas las básicas satisfacen el predicado.
 allDib :: Pred a -> Dibujo a -> Bool
 allDib pre d = foldDib
                   pre
@@ -88,21 +69,28 @@ allDib pre d = foldDib
                   (andIgnoreFloat)
                   (&&)
                   d
+-----------------------------------------------------------------------------------------------------------------------
 
+-----------------------------------------------------------------------------------------------------------------------
 -- Auxiliar de andP.
 andP' :: Pred a -> Pred a -> a -> Bool
 andP' p1 p2 x = p1 x && p2 x
--- Los dos predicados se cumplen para el elemento recibido.
+-- NOTE: Los dos predicados se cumplen para el elemento recibido.
 andP :: Pred a -> Pred a -> Pred a
 andP p1 p2 = (andP' p1 p2) -- Currificación
+-----------------------------------------------------------------------------------------------------------------------
 
+-----------------------------------------------------------------------------------------------------------------------
 -- Auxiliar de orP.
 orP' :: Pred a -> Pred a -> a -> Bool
 orP' p1 p2 x = p1 x || p2 x
--- Algún predicado se cumple para el elemento recibido.
+-- NOTE: Algún predicado se cumple para el elemento recibido.
 orP :: Pred a -> Pred a -> Pred a
 orP p1 p2 = (orP' p1 p2) -- Currificación
+-----------------------------------------------------------------------------------------------------------------------
 
--- Falla siempre es True.
+-----------------------------------------------------------------------------------------------------------------------
+-- NOTE: Falla siempre es True.
 falla :: Bool
 falla = True
+-----------------------------------------------------------------------------------------------------------------------
